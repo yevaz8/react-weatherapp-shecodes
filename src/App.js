e7ee36ddc55e404fb256a953c626db3bb5df85f4
yevaz8 <yesivaz08@gmail.com>
2023-03-08 10:30:13 -0300

    apply math rpund in the wind

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultcity="London" />
+        <Weather defaultcity="Salto" />
       </div>
     </div>
   );

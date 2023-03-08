f48fcc51ed5ac78588ebd4e98d57cf09bf29de5f
yevaz8 <yesivaz08@gmail.com>
2023-03-08 10:10:44 -0300

    update code for weatherindo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -5,25 +5,30 @@ import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="weatherInfo">
+    <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
           <FormatedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize"> {props.data.description} </li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="float-left">
-            <WeatherIcon code={props.data.icon} />
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
+
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
-          <WeatherTemperature code={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
+            <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
-            <li>Humidity: {props.data.humidity} %</li>
           </ul>
         </div>
       </div>

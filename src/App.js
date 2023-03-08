1e4ca3396ea2c1875398c40e9e188b4e38a92bc6
yevaz8 <yesivaz08@gmail.com>
2023-03-08 14:11:49 -0300

    add footer with personal links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,25 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather defaultcity="Salto" />
+        <footer>
+          This projects was coded by Yesica Vázquez and is{" "}
+          <a
+            href="https://github.com/yevaz8/react-weatherapp-shecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://master--incomparable-pika-e40f94.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            on Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );

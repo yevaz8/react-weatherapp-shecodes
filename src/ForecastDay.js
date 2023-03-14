e3c301b1e7a747fe0ea7567b960991b088faa02b
yevaz8 <yesivaz08@gmail.com>
2023-03-14 15:37:39 -0300

    added functions to the forecast component

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,7 +1,19 @@
 import React from "react";
 import WeatherIcon from "./weatherIcon";
+import axios from "axios";
 
-export default function ForecastDay() {
+export default function ForecastDay(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = `eaf223fbefa74d0f073135b8f2023cf9`;
+  let latitude = props.coordinates.lat;
+  let longitude = props.coordinates.lon;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="weatherForecast">
       <div className="row">
@@ -11,7 +23,7 @@ export default function ForecastDay() {
           <div className="forecastTemperatures">
             <span className="weatherForecast-tempertaure-max">19º </span>
             <span className="weatherForecast-tempertaure-min">10º </span>
-          </div>{" "}
+          </div>
         </div>
       </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -59,7 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <ForecastDay />
+        <ForecastDay coordinates={weatherData.coordinates} />
       </div>
     );
   } else {

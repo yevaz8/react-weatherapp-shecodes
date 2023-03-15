2b72f2de92ccd433d065d01d75b50b7c9c580f5e
yevaz8 <yesivaz08@gmail.com>
2023-03-15 14:13:24 -0300

    forecast day building

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -16,9 +16,15 @@ export default function ForecastDay(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
